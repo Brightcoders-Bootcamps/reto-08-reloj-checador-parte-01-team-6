73df92ad712a2ae5e92268bff95781b021bb4fd2
Andres Pacheco <andrespach4@gmail.com>
2020-08-19 23:27:06 -0500

    Create first report

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ReportsController < Admin::ApplicationController
   def index
+    @attendance_by_the_day = WorkCheckup.where('created_at >= ? and created_at <=?', Date.today.midnight, Date.today.end_of_day)
   end
 end
--- app/models/work_checkup.rb
+++ app/models/work_checkup.rb
@@ -1,4 +1,6 @@
 class WorkCheckup < ApplicationRecord
   validates :private_number, presence: true
   validates :employee_id, presence: true
+
+  belongs_to :employee
 end
