935a5e5b9d1a2d06cda51eea98f48df8156ef0f6
Andres Pacheco <andrespach4@gmail.com>
2020-08-20 22:42:23 -0500

    Fix tests

--- test/controllers/company_branches_controller_test.rb
+++ test/controllers/company_branches_controller_test.rb
@@ -3,46 +3,47 @@ require 'test_helper'
 class CompanyBranchesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @company_branch = company_branches(:one)
+    sign_in users(:regular)
   end
 
   test "should get index" do
-    get company_branches_url
+    get admin_company_branches_url
     assert_response :success
   end
 
   test "should get new" do
-    get new_company_branch_url
+    get new_admin_company_branch_url
     assert_response :success
   end
 
   test "should create company_branch" do
     assert_difference('CompanyBranch.count') do
-      post company_branches_url, params: { company_branch: { address: @company_branch.address, name: @company_branch.name } }
+      post admin_company_branches_url, params: { company_branch: { address: @company_branch.address, name: @company_branch.name } }
     end
 
-    assert_redirected_to company_branch_url(CompanyBranch.last)
+    assert_redirected_to admin_company_branches_url
   end
 
   test "should show company_branch" do
-    get company_branch_url(@company_branch)
+    get admin_company_branch_url(@company_branch)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_company_branch_url(@company_branch)
+    get edit_admin_company_branch_url(@company_branch)
     assert_response :success
   end
 
   test "should update company_branch" do
-    patch company_branch_url(@company_branch), params: { company_branch: { address: @company_branch.address, name: @company_branch.name } }
-    assert_redirected_to company_branch_url(@company_branch)
+    patch admin_company_branch_url(@company_branch), params: { company_branch: { address: @company_branch.address, name: @company_branch.name } }
+    assert_redirected_to admin_company_branches_url
   end
 
   test "should destroy company_branch" do
     assert_difference('CompanyBranch.count', -1) do
-      delete company_branch_url(@company_branch)
+      delete admin_company_branch_url(@company_branch)
     end
 
-    assert_redirected_to company_branches_url
+    assert_redirected_to admin_company_branches_url
   end
 end
--- test/controllers/employees_controller_test.rb
+++ test/controllers/employees_controller_test.rb
@@ -1,48 +1,42 @@
 require 'test_helper'
 
 class EmployeesControllerTest < ActionDispatch::IntegrationTest
+
   setup do
     @employee = employees(:one)
+    sign_in users(:regular)
   end
 
   test "should get index" do
-    get employees_url
+    get admin_employees_url
     assert_response :success
   end
 
   test "should get new" do
-    get new_employee_url
+    get new_admin_employee_url
     assert_response :success
   end
 
   test "should create employee" do
     assert_difference('Employee.count') do
-      post employees_url, params: { employee: { email: @employee.email, name: @employee.name, position: @employee.position, private_number: @employee.private_number } }
+      post admin_employees_url, params: { employee: { email: "test@test.io", name: @employee.name, position: @employee.position, private_number: '12345' } }
     end
 
-    assert_redirected_to employee_url(Employee.last)
+    assert_redirected_to admin_employees_url
   end
 
   test "should show employee" do
-    get employee_url(@employee)
+    get admin_employee_url(@employee)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_employee_url(@employee)
+    get edit_admin_employee_url(@employee)
     assert_response :success
   end
 
   test "should update employee" do
-    patch employee_url(@employee), params: { employee: { email: @employee.email, name: @employee.name, position: @employee.position, private_number: @employee.private_number } }
-    assert_redirected_to employee_url(@employee)
-  end
-
-  test "should destroy employee" do
-    assert_difference('Employee.count', -1) do
-      delete employee_url(@employee)
-    end
-
-    assert_redirected_to employees_url
+    patch admin_employee_url(@employee), params: { employee: { email: 'test@test.io', name: @employee.name, position: '12345', private_number: @employee.private_number } }
+    assert_redirected_to admin_employees_url
   end
 end
