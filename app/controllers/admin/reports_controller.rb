dc8e05471ebec9f1d312e730609466ced14790cd
Andres Pacheco <andrespach4@gmail.com>
2020-08-20 22:15:45 -0500

    Add reports

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ReportsController < Admin::ApplicationController
   def index
     @attendance_by_the_day = WorkCheckup.where('created_at >= ? and created_at <=?', Date.today.midnight, Date.today.end_of_day)
+    @average_time = WorkCheckup.where('created_at >= ? and created_at <=?', Date.today.at_beginning_of_month, Date.today.at_end_of_month).group(:private_number).count
   end
 end
