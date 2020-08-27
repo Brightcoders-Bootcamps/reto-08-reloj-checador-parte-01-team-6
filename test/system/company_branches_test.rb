878a3f10f17deb7fe14b9f44e18e98d05c28de75
Andres Pacheco <andrespach4@gmail.com>
2020-08-27 18:32:55 -0500

    Fixing test

--- test/system/company_branches_test.rb
+++ test/system/company_branches_test.rb
@@ -3,15 +3,16 @@ require "application_system_test_case"
 class CompanyBranchesTest < ApplicationSystemTestCase
   setup do
     @company_branch = company_branches(:one)
+    sign_in users(:regular)
   end
 
   test "visiting the index" do
-    visit company_branches_url
+    visit admin_company_branches_url
     assert_selector "h1", text: "Company Branches"
   end
 
   test "creating a Company branch" do
-    visit company_branches_url
+    visit admin_company_branches_url
     click_on "New Company Branch"
 
     fill_in "Address", with: @company_branch.address
@@ -19,11 +20,11 @@ class CompanyBranchesTest < ApplicationSystemTestCase
     click_on "Create Company branch"
 
     assert_text "Company branch was successfully created"
-    click_on "Back"
+    
   end
 
   test "updating a Company branch" do
-    visit company_branches_url
+    visit admin_company_branches_url
     click_on "Edit", match: :first
 
     fill_in "Address", with: @company_branch.address
@@ -31,11 +32,10 @@ class CompanyBranchesTest < ApplicationSystemTestCase
     click_on "Update Company branch"
 
     assert_text "Company branch was successfully updated"
-    click_on "Back"
   end
 
   test "destroying a Company branch" do
-    visit company_branches_url
+    visit admin_company_branches_url
     page.accept_confirm do
       click_on "Destroy", match: :first
     end
